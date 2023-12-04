48ef702362a0f7683ab6ff1cbac02467e01c4c8d
ChristJohnson <cmj21md@gmail.com>
2023-12-04 02:56:00 -0500

    some metadata work and added lib

---
+++ rpskills/RPSkillsMod.cs
@@ -0,0 +1,31 @@
+﻿using Vintagestory.API.Common;
+using Vintagestory.API.Server;
+
+namespace rpskills
+{
+    /// <summary>
+    /// ModSystem is the base for any VintageStory code mods
+    /// </summary>
+    public class RPSkillsMod : ModSystem
+    {
+        /// <summary>
+        /// Utility for accessing the server's implemented functionality.
+        /// 
+        /// "Object Reference to the ICoreServerAPI Interface"
+        /// </summary>
+        ICoreServerAPI sapi;
+
+        /// <summary>
+        /// Server initialization
+        /// </summary>
+        /// <param name="api">Utility for accessing the server's functionality</param>
+        public override void StartServerSide(ICoreServerAPI api)
+        {
+            base.StartServerSide(api);
+            this.sapi = api;
+
+
+        }
+
+    }
+}
