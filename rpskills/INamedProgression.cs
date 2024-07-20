22558fb2aceb67ec77d7c764922cd6764b49204a
ChristJohnson <cmj21md@gmail.com>
2024-07-20 06:26:23 -0400

    big refactor; works

---
+++ rpskills/INamedProgression.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace origins
+{
+    internal interface INamedProgression
+    {
+        string Name { get; }
+    }
+}
---
+++ rpskills/SkillSystem.cs
@@ -0,0 +1,71 @@
+﻿using rpskills;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vintagestory.API.Client;
+using Vintagestory.API.Common;
+using Vintagestory.API.Server;
+
+namespace origins
+{
+    internal class SkillSystem : ProgressionSystem<Skill>
+    {
+        internal static void Build(ICoreAPI api)
+        {
+            ProgressionSystem<Origin>.Load(api, "origins:config/skills.json");
+
+            api.Logger.Event("loaded skills");
+        }
+
+        public static Skill GetSkill(string name)
+        {
+            return EntriesByName[name];
+        }
+
+
+        private PlayerSkillsUI PlayerSkillsUI;
+
+        public SkillSystem(ICoreAPI api) : base(api)
+        {
+        }
+
+        internal override void ClientInit(ICoreClientAPI capi)
+        {
+            //Note(Moon):
+            //these lines are what's needed in order to turn the dialog box, the initialization
+            //of the PlayerSkillsUI can be moved to a seprate class and likely will be at a later date.
+            //It just needs the capi in order to be be hooked for the hotkey.
+            PlayerSkillsUI = new PlayerSkillsUI(capi);
+            capi.Input.RegisterHotKey("Skill Interface", "Opens up the Skills GUI", GlKeys.O, HotkeyType.GUIOrOtherControls);
+            capi.Input.SetHotKeyHandler("Skill Interface", ToggleGUI);
+        }
+
+        internal override void ServerInit(ICoreServerAPI sapi)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool ToggleGUI(KeyCombination comb)
+        {
+            if (PlayerSkillsUI.IsOpened())
+                PlayerSkillsUI.TryClose();
+            else
+                PlayerSkillsUI.TryOpen();
+
+            return true;
+        }
+    }
+
+    public class Skill : INamedProgression
+    {
+        public string Name;
+
+        public int Level;
+
+        public List<string> Paths;
+
+        string INamedProgression.Name => Name;
+    }
+}
